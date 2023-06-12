019b8448006e662170acdaac84dc4ee0ade223e3
Jiří Andrlík <andrlik.jirka@gmail.com>
2023-06-12 19:36:43 +0200

    oprava warnings, rozdělení do souborů

---
+++ figurky.pl
@@ -0,0 +1,24 @@
+% barvy figurek / hráčů
+barva(bila).
+barva(cerna).
+
+barva_hraci(cerna,bila).
+barva_hraci(bila,cerna).
+
+
+% figurky na šachovnici (označení, barva, znak pro výpis, ohodnocení)
+figurka(b_kral,bila,'\u2654',(-900)).
+figurka(b_dama,bila,'\u2655',(-90)).
+figurka(b_vez,bila,'\u2656',(-50)).
+figurka(b_jezdec,bila,'\u2658',(-30)).
+figurka(b_strelec,bila,'\u2657',(-30)).
+figurka(b_pesec,bila,'\u2659',(-10)).
+
+
+figurka(c_kral,cerna,'\u265a',900).
+figurka(c_dama,cerna,'\u265b',90).
+figurka(c_vez,cerna,'\u265c',50).
+figurka(c_jezdec,cerna,'\u265e',30).
+figurka(c_strelec,cerna,'\u265d',30).
+figurka(c_pesec,cerna,'\u265f',10).
+
--- sachy.pl
+++ sachy.pl
@@ -1,96 +1,7 @@
-barva(bila).
-barva(cerna).
+:- consult(figurky).
+:- consult(pozice).
 
-barva_hraci(cerna,bila).
-barva_hraci(bila,cerna).
-
-figurka(b_kral,bila,'\u2654',(-900)).
-figurka(b_dama,bila,'\u2655',(-90)).
-figurka(b_vez,bila,'\u2656',(-50)).
-figurka(b_jezdec,bila,'\u2658',(-30)).
-figurka(b_strelec,bila,'\u2657',(-30)).
-figurka(b_pesec,bila,'\u2659',(-10)).
-
-
-figurka(c_kral,cerna,'\u265a',900).
-figurka(c_dama,cerna,'\u265b',90).
-figurka(c_vez,cerna,'\u265c',50).
-figurka(c_jezdec,cerna,'\u265e',30).
-figurka(c_strelec,cerna,'\u265d',30).
-figurka(c_pesec,cerna,'\u265f',10).
-
-
-pozice(a1,1,1).
-pozice(a2,1,2).
-pozice(a3,1,3).
-pozice(a4,1,4).
-pozice(a5,1,5).
-pozice(a6,1,6).
-pozice(a7,1,7).
-pozice(a8,1,8).
-
-pozice(b1,2,1).
-pozice(b2,2,2).
-pozice(b3,2,3).
-pozice(b4,2,4).
-pozice(b5,2,5).
-pozice(b6,2,6).
-pozice(b7,2,7).
-pozice(b8,2,8).
-
-pozice(c1,3,1).
-pozice(c2,3,2).
-pozice(c3,3,3).
-pozice(c4,3,4).
-pozice(c5,3,5).
-pozice(c6,3,6).
-pozice(c7,3,7).
-pozice(c8,3,8).
-
-pozice(d1,4,1).
-pozice(d2,4,2).
-pozice(d3,4,3).
-pozice(d4,4,4).
-pozice(d5,4,5).
-pozice(d6,4,6).
-pozice(d7,4,7).
-pozice(d8,4,8).
-
-pozice(e1,5,1).
-pozice(e2,5,2).
-pozice(e3,5,3).
-pozice(e4,5,4).
-pozice(e5,5,5).
-pozice(e6,5,6).
-pozice(e7,5,7).
-pozice(e8,5,8).
-
-pozice(f1,6,1).
-pozice(f2,6,2).
-pozice(f3,6,3).
-pozice(f4,6,4).
-pozice(f5,6,5).
-pozice(f6,6,6).
-pozice(f7,6,7).
-pozice(f8,6,8).
-
-pozice(g1,7,1).
-pozice(g2,7,2).
-pozice(g3,7,3).
-pozice(g4,7,4).
-pozice(g5,7,5).
-pozice(g6,7,6).
-pozice(g7,7,7).
-pozice(g8,7,8).
-
-pozice(h1,8,1).
-pozice(h2,8,2).
-pozice(h3,8,3).
-pozice(h4,8,4).
-pozice(h5,8,5).
-pozice(h6,8,6).
-pozice(h7,8,7).
-pozice(h8,8,8).
+:- public(sachy/0).
 
 start_sachovnice(Sachovnice) :-
     Sachovnice = [
@@ -169,7 +80,7 @@ kontrola_vstup_odkud_figurka(Odkud,Sachovnice,BarvaHrac) :-
 kontrola_vstup_kam_figurka(Kam,Sachovnice,BarvaHrac) :-
     pozice(Kam,_,_),
     (
-        not(member([Kam,Figurka],Sachovnice)); % na cílové pozici není žádná figurka
+        not(member([Kam,_],Sachovnice)); % na cílové pozici není žádná figurka
         (
         member([Kam,Figurka],Sachovnice), % na cílové pozici je figurka
         not(figurka(Figurka,BarvaHrac,_,_)) % ale je opačné barvy
@@ -212,7 +123,7 @@ vstup_tah(Odkud,Kam,Sachovnice,Barva) :-
 
 
 % kontrola tahů
-kontrola_tah(Odkud,Kam,Sachovnice,BarvaHrac) :-
+kontrola_tah(Odkud,Kam,Sachovnice,_) :-
     pozice(Odkud,OdkudX,OdkudY),
     pozice(Kam,KamX,KamY),
     member([Odkud,Figurka],Sachovnice),
@@ -272,12 +183,12 @@ kontrola_tah_(c_strelec,OdkudX,OdkudY,KamX,KamY,Sachovnice) :-
     abs(KamX-OdkudX) =:= abs(KamY-OdkudY),
     neobsazeno_diagonalne(OdkudX,OdkudY,KamX,KamY,Sachovnice).
 
-kontrola_tah_(b_jezdec,OdkudX,OdkudY,KamX,KamY,Sachovnice) :-
+kontrola_tah_(b_jezdec,OdkudX,OdkudY,KamX,KamY,_) :-
     RozdilX is abs(OdkudX-KamX),
     RozdilY is abs(OdkudY-KamY),
     ((RozdilX=:=2,RozdilY=:=1);(RozdilX=:=1,RozdilY=:=2)).
 
-kontrola_tah_(c_jezdec,OdkudX,OdkudY,KamX,KamY,Sachovnice) :-
+kontrola_tah_(c_jezdec,OdkudX,OdkudY,KamX,KamY,_) :-
     RozdilX is abs(OdkudX-KamX),
     RozdilY is abs(OdkudY-KamY),
     ((RozdilX=:=2,RozdilY=:=1);(RozdilX=:=1,RozdilY=:=2)).
@@ -333,7 +244,7 @@ kontrola_tah_(c_pesec,OdkudX,OdkudY,KamX,KamY,Sachovnice) :-
 
 % kontrola neobsazenosti polí na cestě
 
-neobsazeno_vodorovne(OdkudX,OdkudY,KamX,KamY,Sachovnice) :-
+neobsazeno_vodorovne(OdkudX,OdkudY,KamX,_,Sachovnice) :-
     OdkudX =\= KamX,
     X1 is min(OdkudX,KamX)+1,
     X2 is max(OdkudX,KamX),
@@ -341,7 +252,7 @@ neobsazeno_vodorovne(OdkudX,OdkudY,KamX,KamY,Sachovnice) :-
     findall(Poz,(member(Poz,KontrolPozice),member([Poz,_],Sachovnice)),ObsazenePozice),
     length(ObsazenePozice,0).
 
-neobsazeno_svisle(OdkudX,OdkudY,KamX,KamY,Sachovnice) :- 
+neobsazeno_svisle(OdkudX,OdkudY,_,KamY,Sachovnice) :- 
     OdkudY =\= KamY,
     Y1 is min(OdkudY,KamY)+1,
     Y2 is max(OdkudY,KamY),
